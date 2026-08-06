441fc42e857eebdf508b762c1668d51a9a7834ff
MEnglund92 <mattias.englund1992@gmail.com>
2026-08-06 10:03:08 +0200

    Home redesign: concept-style greeting, 2x2 stat grid, course cards first; SW cache v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'english-study-v4';
+const CACHE = 'english-study-v5';
 const FILES = ['.','index.html','data.js','manifest.json','icons/icon-192.png','icons/icon-512.png'];
 self.addEventListener('install',e=>{
  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
